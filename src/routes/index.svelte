de7931af0c7e5a7d6796a2e44812b85fa8cbf916
ProcessX <luc2526@gmail.com>
2022-02-15 22:37:07 +0100

    basic-template::grid container set up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,23 +8,43 @@
 </svelte:head>
 
 <section>
-  <h1 class="heading heading-1">Un patro jeune près de chez toi</h1>
-  <p>Lance-toi dans un mouvement de jeunesse qui bouge.</p>
-  <p>Ouvert à tous, pour les enfants de 5 à 18 ans.</p>
-  <div>
-    <a href="">S'inscrire</a>
-    <a href="">Nous contacter</a>
+  <div class="grid grid-container">
+    <div class="col-span-1">
+      <p>l</p>
+    </div>
+    <div class="col-span-25">
+      <p>a</p>
+    </div>
+  </div>
+  <div class="grid-container">
+    <div class="row">
+      <h1 class="heading heading-1">
+        Un patro jeune près de <spans class="text-lime-500">chez toi</spans>
+      </h1>
+      <p class="paragraph paragraph-base">
+        Lance-toi dans un mouvement de jeunesse qui bouge.
+      </p>
+      <p class="paragraph paragraph-base">
+        Ouvert à tous, pour les enfants de 5 à 18 ans.
+      </p>
+      <div>
+        <a href="">S'inscrire</a>
+        <a href="">Nous contacter</a>
+      </div>
+    </div>
   </div>
 </section>
 
 <section>
-  <h1>Le patro, c'est quoi&nbsp?</h1>
-  <p>Un mouvement de jeunesse pour tous.</p>
-  <p>
+  <h2 class="heading heading-2">Le patro, c'est quoi&nbsp?</h2>
+  <p class="heading heading-5 text-amber-500">
+    Un mouvement de jeunesse pour tous.
+  </p>
+  <p class="paragraph paragraph-base">
     Au patro de Glimes, nous nous rassemblons un samedi sur deux pour jouer et
     rire ensemble.
   </p>
-  <p>
+  <p class="paragraph paragraph-base">
     Peu importe ton âge, si tu veux faire des rencontres dans la bonne humeur,
     notre patro est fait pour toi.
   </p>
@@ -35,9 +55,11 @@
 </section>
 
 <section>
-  <h1>Viens essayer&nbsp!</h1>
-  <p>Une après-midi gratuite, sans inscription.</p>
-  <p>
+  <h2 class="heading heading-2">Viens essayer&nbsp!</h2>
+  <p class="heading heading-5 text-lime-500">
+    Une après-midi gratuite, sans inscription.
+  </p>
+  <p class="paragraph paragraph-base">
     Vous hésitez ? Pour que chaque enfant puisse se faire une idée, nous vous
     offrons une après-midi d’animation. Pas besoin de s’inscrire, vous pouvez
     même passer à l’improviste.
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,11 @@
+const breakpoints = {
+  mobile: 480,
+  tablet: 640,
+  laptop: 998,
+  desktop: 1280,
+  retina: 1560
+}
+
 const fontRatio = {
   mobile: 1.2,
   tablet: 1.25,
@@ -14,18 +22,26 @@ const fontSizeNames = {
 function generateFontSizes() {
   let fontSize = {};
   for (const [screenSize, ratio] of Object.entries(fontRatio)) {
-    console.log(`${screenSize}: ${ratio}`);
     for (let i = 0; i < fontSizeNames.crescent.length; i++) {
-      console.log(ratio ** i);
       fontSize[`${screenSize}-${fontSizeNames.crescent[i]}`] = `${ratio ** i}rem`;
     }
     for (let i = 0; i < fontSizeNames.decrescent.length; i++) {
       fontSize[`${screenSize}-${fontSizeNames.decrescent[i]}`] = `${ratio ** -(i + 1)}rem`;
     }
   };
-  console.log(fontSize);
   return fontSize;
 }
+
+function generateGridContainers() {
+  let gridContainers = {};
+  for (const bp of Object.keys(breakpoints)) {
+    gridContainers[`container-${bp}`] = `max(1rem, calc((100% - (${breakpoints[bp]}px - 2rem)) / 2)) repeat(25, calc((min(${breakpoints[bp]}px, 100%) - 2rem) / 25)) max(1rem, calc((100% - (${breakpoints[bp]}px - 2rem)) / 2))`;
+
+  }
+  return gridContainers;
+}
+
+generateGridContainers();
 
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -34,41 +50,51 @@ module.exports = {
       body: ['Lato', 'ui-sans-serif'],
       heading: ['Montserrat', 'ui-sans-serif']
     },
-    colors: {
-      primary: {
-        "50": "#F4F8EC",
-        "100": "#E0ECCA",
-        "200": "#CBE0A9",
-        "300": "#B7D487",
-        "400": "#A3C865",
-        "500": "#8FBC43",
-        "600": "#729636",
-        "700": "#567128",
-        "800": "#394B1B",
-        "900": "#1D260D"
-      },
-      secondary: {
-        "50": "#FEF6E6",
-        "100": "#FDE6BA",
-        "200": "#FCD68D",
-        "300": "#FAC561",
-        "400": "#F9B534",
-        "500": "#F8A507",
-        "600": "#C68406",
-        "700": "#956304",
-        "800": "#634203",
-        "900": "#322101"
-      },
-    },
+
     screens: {
-      'tablet': '640px',
-      'laptop': '998px',
-      'desktop': '1280px',
-      'retina': '1560px',
+      'tablet': `${breakpoints.tablet}px`,
+      'laptop': `${breakpoints.laptop}px`,
+      'desktop': `${breakpoints.desktop}px`,
+      'retina': `${breakpoints.retina}px`,
     },
     fontSize: generateFontSizes(),
     extend: {
-
+      colors: {
+        primary: {
+          "50": "#F4F8EC",
+          "100": "#E0ECCA",
+          "200": "#CBE0A9",
+          "300": "#B7D487",
+          "400": "#A3C865",
+          "500": "#8FBC43",
+          "600": "#729636",
+          "700": "#567128",
+          "800": "#394B1B",
+          "900": "#1D260D"
+        },
+        secondary: {
+          "50": "#FEF6E6",
+          "100": "#FDE6BA",
+          "200": "#FCD68D",
+          "300": "#FAC561",
+          "400": "#F9B534",
+          "500": "#F8A507",
+          "600": "#C68406",
+          "700": "#956304",
+          "800": "#634203",
+          "900": "#322101"
+        },
+      },
+      lineHeight: {
+        'paragraph': '1.6em',
+        'heading': '1.4em'
+      },
+      gridTemplateColumns: {
+        ...generateGridContainers(),
+      },
+      gridColumn: {
+        'span-25': '2 / -2'
+      }
     },
   },
   plugins: [],
