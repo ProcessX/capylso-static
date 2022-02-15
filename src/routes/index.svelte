5016c8ca853d48313068e5f8665fde9f7a601eee
ProcessX <luc2526@gmail.com>
2022-02-15 11:54:40 +0100

    basic-template::first commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,7 +13,22 @@
 <section>
 	<h1>Un patro jeune près de chez toi</h1>
 	<p>Lance-toi dans un mouvement de jeunesse qui bouge.</p>
-	<p>Ouvert</p>
+	<p>Ouvert à tous, pour les enfants de 5 à 18 ans.</p>
+	<div>
+		<a href="#">S'inscrire</a>
+		<a href="#">Nous contacter</a>
+	</div>
+</section>
+
+<section>
+	<h1>Le patro, c'est quoi ?</h1>
+	<p>Un mouvement de jeunesse pour tous</p>
+	<p>Au patro de Glimes, nous nous rassemblons un samedi sur deux pour jouer et rire ensemble.</p>
+	<p>Peu importe ton âge, si tu veux faire des rencontres dans la bonne humeur, notre patro est fait pour toi.</p>
+	<div>
+		<a href="#">S'inscrire</a>
+		<a href="#">Nous contacter</a>
+	</div>
 </section>
 
 <style>
